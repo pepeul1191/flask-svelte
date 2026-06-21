bd45477cd90f6ea69362e5e773cf33770ce078d1
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2026-06-21 10:58:01 -0500

    exluyendo apis de la procetccion csrf

--- auth/apis.py
+++ auth/apis.py
@@ -1,8 +1,16 @@
 # auth/apis.py
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 apis = Blueprint('auth-apis', __name__)
 
 @apis.route('/api/v1/demo', methods=['GET'])
-def demo():
-  return 'hola mundo', 200
+def demo_get():
+  return jsonify({
+      'message': 'hola GET'
+    }), 200
+
+@apis.route('/api/v1/demo', methods=['POST'])
+def demo_post():
+  return jsonify({
+      'message': 'hola POST'
+    }), 200
--- main/blueprints.py
+++ main/blueprints.py
@@ -1,7 +1,11 @@
 # main/blueprints.py
 # from admin.blueprints import blueprints as admin_blueprints
+from main.application import csrf
+# modules blueprints
 from web.blueprints import blueprints as web_blueprints
 from auth.blueprints import blueprints as auth_blueprints
+# doules apis for csrf excluding
+from auth.apis import apis as auth_apis
 
 def register(app):
   # append sub blueprints
@@ -14,4 +18,5 @@ def register(app):
   for blueprints in modules_blueprints:
     for blueprint in blueprints:
       app.register_blueprint(blueprint)
-
+  # Excluir blueprint API del CSRF
+  csrf.exempt(auth_apis)
